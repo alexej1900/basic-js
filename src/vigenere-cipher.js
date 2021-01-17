c717ca26f0f8cc10f523451d296751af6d1e3edb
Alexej Bodnarchuk <alexej1900@mail.ru>
2021-01-17 11:48:07 +0300

    feat: solution vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -4,42 +4,67 @@ class VigenereCipheringMachine {
     constructor(mode = true) {
         this.mode = mode;
     }
-    encrypt(str, key) {
-        //if (this.mode) {
-        let arr = 'abcdefghijklmnopqrstuvwxyz';
-        let criptArr = [];
-        while (str.length > 0) {
-            for (let i = 0; i < key.length; i++) {
-                if (typeof str[i] !== 'undefined' && arr.includes(str[i])) {
-                    criptArr.push(arr[(arr.indexOf(str[i]) + arr.indexOf(key[i])) % 26]);
-                } else if (!arr.includes(str[i])) {
-                    criptArr.push(str[i]);
-                }
-            }
-            str = (str.length > key.length) ? str.slice(key.length) : 0;
+    encrypt(str, keys) {
+        if (str === undefined || keys === undefined) throw new Error();
+        let message = str.toUpperCase();
+        let key = keys.toUpperCase();
+        let reg = /[A-Z]/;
+        let mesArr = [];
+        let keyArr = [];
+        let mesEncrypt = [];
+        for (let i of message) {
+            if (reg.test(i) === true) mesArr.push(i.charCodeAt() - 65);
         }
-        return criptArr.join('').toUpperCase();
-        //}
+        while (keyArr.length <= mesArr.length) {
+            for (let i of key) keyArr.push(i.charCodeAt() - 65);
+        }
+        if (keyArr.length > mesArr.length) keyArr = keyArr.slice(0, mesArr.length);
+        for (let i = 0; i < mesArr.length; i += 1) {
+            mesArr[i] = String.fromCharCode((mesArr[i] + keyArr[i]) % 26 + 65);
+        }
+        let a = 0;
+        for (let i of message) {
+            if (reg.test(i) === true) {
+                i = mesArr[a];
+                a += 1;
+            }
+            mesEncrypt.push(i);
+        }
+        if (this.mode === true) return mesEncrypt.join("");
+        return mesEncrypt.reverse().join("");
     }
-    decrypt() {
-        if (!this.mode) {
-            let arr = 'abcdefghijklmnopqrstuvwxyz';
-            let cripotArr = [];
-            while (str.length > 0) {
-                for (let i = 0; i < key.length; i++) {
-                    if ((typeof str[i] !== 'undefined') && arr.includes(str[i])) {
-                        cripotArr.push(arr[((arr.indexOf(str[i]) + 26) - arr.indexOf(key[i])) % 26]);
-
-                    } else if (!arr.includes(str[i])) {
-                        cripotArr.push(str[i]);
-                    }
-                }
-                str = (str.length > key.length) ? str.slice(key.length) : 0;
-            }
-            return cripotArr.join('').toUpperCase();
+    decrypt(str, keys) {
+        if (str === undefined || keys === undefined) throw new Error();
+        let message = str.toUpperCase();
+        let key = keys.toUpperCase();
+        let reg = /[A-Z]/;
+        let mesArr = [];
+        let keyArr = [];
+        let mesDecrypt = [];
+        for (let i of message) {
+            if (reg.test(i) === true) mesArr.push(i.charCodeAt() - 65);
         }
-
+        while (keyArr.length <= mesArr.length) {
+            for (let i of key) keyArr.push(i.charCodeAt() - 65);
+        }
+        if (keyArr.length > mesArr.length) keyArr = keyArr.slice(0, mesArr.length);
+        for (let i = 0; i < mesArr.length; i += 1) {
+            mesArr[i] = mesArr[i] + 65 - keyArr[i];
+            if (mesArr[i] < 65) {
+                while (mesArr[i] < 65) mesArr[i] = mesArr[i] + 26;
+            }
+            mesArr[i] = String.fromCharCode((mesArr[i]));
+        }
+        let a = 0;
+        for (let i of message) {
+            if (reg.test(i) === true) {
+                i = mesArr[a];
+                a += 1;
+            }
+            mesDecrypt.push(i);
+        }
+        if (this.mode === true) return mesDecrypt.join("");
+        return mesDecrypt.reverse().join("");
     }
 }
-
 module.exports = VigenereCipheringMachine;
